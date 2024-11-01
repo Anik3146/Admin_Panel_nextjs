9aa7efb2dba9eaaacebdfb85c8a1595c0917cf6c
Anik3146 <anikweb153@gmail.com>
2024-11-01 13:05:56 +0600

    used remotePattern in next.config for image domain

--- next.config.mjs
+++ next.config.mjs
@@ -6,32 +6,37 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'cdn.sanity.io',
         port: '',
-        pathname: '/**', // Adjust the pathname as needed
+        pathname: '/**', // Adjust if needed
       },
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
         port: '',
-        pathname: '/**', // Adjust the pathname as needed
+        pathname: '/**', // Adjust if needed
       },
       {
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
         port: '',
-        pathname: '/**', // Adjust the pathname as needed
+        pathname: '/**', // Adjust if needed
       },
       {
         protocol: 'https',
         hostname: 'pub-b7fd9c30cdbf439183b75041f5f71b92.r2.dev',
         port: '',
-        pathname: '/**', // Adjust the pathname as needed
+        pathname: '/**', // Adjust if needed
       },
-      // If you need to support localhost for development, you can add a pattern for it
+      {
+        protocol: 'https',
+        hostname: 'www.chanel.com', // Add this line for Chanel
+        port: '',
+        pathname: '/images/**', // Adjust if you want to restrict the paths
+      },
       {
         protocol: 'http',
         hostname: 'localhost',
         port: '',
-        pathname: '/**', // Adjust the pathname as needed
+        pathname: '/**', // For local development
       },
     ],
   },
