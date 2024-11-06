fe91c21676db79ecb4e05869cf9ac209e2e1e017
Anik3146 <anikweb153@gmail.com>
2024-11-06 22:39:30 +0600

    coupon page api's added

--- src/app/coupons/page.tsx
+++ src/app/coupons/page.tsx
@@ -1,21 +1,16 @@
 'use client';
 
 import Breadcrumb from '@/components/Breadcrumbs/Breadcrumb';
-import TableOne from '@/components/Tables/TableOne';
-import TableThree from '@/components/Tables/TableThree';
-import TableTwo from '@/components/Tables/TableTwo';
-
-import { Metadata } from 'next';
 import DefaultLayout from '@/components/Layouts/DefaultLaout';
-import CommonTable from '@/components/Tables/productTable';
+import CouponTable from '@/components/Tables/couponTable';
 
 const ProductsPage = () => {
   return (
     <DefaultLayout>
-      <Breadcrumb pageName="Products" />
+      <Breadcrumb pageName="Coupons" />
 
       <div className="flex flex-col gap-10">
-        <CommonTable />
+        <CouponTable />
       </div>
     </DefaultLayout>
   );
