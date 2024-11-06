e7b9d21bdc27198217c929cecae233536b3fb8a3
RTC-03\User <rtc.anik@gmail.com>
2024-11-06 14:35:09 +0600

    brand page completed

--- src/app/brands/page.tsx
+++ src/app/brands/page.tsx
@@ -1,21 +1,16 @@
 'use client';
 
 import Breadcrumb from '@/components/Breadcrumbs/Breadcrumb';
-import TableOne from '@/components/Tables/TableOne';
-import TableThree from '@/components/Tables/TableThree';
-import TableTwo from '@/components/Tables/TableTwo';
-
-import { Metadata } from 'next';
 import DefaultLayout from '@/components/Layouts/DefaultLaout';
-import CommonTable from '@/components/Tables/productTable';
+import BrandTable from '@/components/Tables/brandTable';
 
 const ProductsPage = () => {
   return (
     <DefaultLayout>
-      <Breadcrumb pageName="Products" />
+      <Breadcrumb pageName="Brands" />
 
       <div className="flex flex-col gap-10">
-        <CommonTable />
+        <BrandTable />
       </div>
     </DefaultLayout>
   );
